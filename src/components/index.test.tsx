9dc61ba0efa53e2968161ec438478c1e1abed80d
knknkn1162 <knknkn1162@gmail.com>
2017-12-11 08:38:27 +0900

    test reducers and action

--- src/components/index.test.tsx
+++ src/components/index.test.tsx
@@ -1,9 +1,53 @@
 import { addTodo, setVisibilityFilter, toggleTodo ,VisibilityFilter } from './actions';
 import { store } from './index';
 import { initialState, State, todoApp } from './reducer';
+import { List } from 'immutable';
 
 it('test store.getState', () => {
-  const state: State = store.getState();
-  const expectedState = initialState;
-  expect(state).toEqual(expectedState);
+  let state: State = store.getState();
+  expect(state).toEqual(initialState);
+
+  store.dispatch(addTodo('Learn about actions'));
+  store.dispatch(addTodo('Learn about reducers'));
+
+  state = store.getState();
+  expect(state).toEqual(
+    {
+      todos: List(
+        [{ completed: false, text: 'Learn about actions' },
+         { completed: false, text: 'Learn about reducers' },
+        ]),
+      visibilityFilter: 'SHOW_ALL',
+    },
+  );
+
+  store.dispatch(toggleTodo(0));
+  store.dispatch(toggleTodo(1));
+
+  state = store.getState();
+
+  expect(state).toEqual(
+    {
+      todos: List(
+        [{ completed: true, text: 'Learn about actions' },
+         { completed: true, text: 'Learn about reducers' },
+        ]),
+      visibilityFilter: 'SHOW_ALL',
+    },
+  );
+
+  store.dispatch(setVisibilityFilter(VisibilityFilter.SHOW_COMPLETED));
+
+  state = store.getState();
+
+  expect(state).toEqual(
+    {
+      todos: List(
+        [{ completed: true, text: 'Learn about actions' },
+          { completed: true, text: 'Learn about reducers' },
+        ]),
+      visibilityFilter: 'SHOW_COMPLETED',
+    },
+  );
+
 });
