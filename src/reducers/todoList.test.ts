87e7756e614dbf1c8def1a20c2e244e4d184c6d5
knknkn1162 <knknkn1162@gmail.com>
2017-12-14 12:52:29 +0900

    fix todoList test

--- src/reducers/todoList.test.ts
+++ src/reducers/todoList.test.ts
@@ -3,7 +3,7 @@ import { initTodoList } from "../states/todoList";
 import { StateType } from "../actions";
 import { List } from "immutable";
 
-it("todo reducer test", () => {
+it("todo toggle-todo test", () => {
   expect(
     todoList(
       initTodoList(["hello", "goodbye"]),
@@ -13,17 +13,17 @@ it("todo reducer test", () => {
     .toEqual(List([
         {completed: false, index: 0, text: "hello"},
         {completed: true, index: 1, text: "goodbye"},
-      ]));
+    ]));
+});
 
+it("todo add_todo test", () => {
   expect(
     todoList(
-      initTodoList(["hello", "goodbye"]),
-      {index: 2, type: StateType.ADD_TODO, text: "see"},
+      initTodoList([]),
+      {index: 0, type: StateType.ADD_TODO, text: "hello"},
     ),
   )
     .toEqual(List([
       {completed: false, index: 0, text: "hello"},
-      {completed: false, index: 1, text: "goodbye"},
-      {completed: false, index: 2, text: "see"},
     ]));
 });
